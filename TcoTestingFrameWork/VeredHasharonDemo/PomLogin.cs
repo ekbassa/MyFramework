afce56c8d0bf57e4eaf7d4dac415c4d4d3b37a8e
bassam <https://github.com/ekbassa/MyFramework.git>
2019-02-18 18:45:18 +0200

    adding a vereddemo Pom

---
+++ TcoTestingFrameWork/VeredHasharonDemo/PomLogin.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using TcoTestingFrameWork.VeredHasharon.POM;
+
+namespace TcoTestingFrameWork.VeredHasharonDemo
+{
+    class PomLogin
+    {
+        private IWebDriver _driver;
+        public PomLogin(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        //IwebElements
+        public IWebElement txt_userName => _driver.FindElement(By.Id("txtUserName"));
+        public IWebElement txt_userpassword => _driver.FindElement(By.Id("txtPassword"));
+        public IWebElement btn_Login => _driver.FindElement(By.Id("btnLogIn"));
+
+
+        //Action 
+        public void Login(string userName,string passWord)
+        {
+            txt_userName.SendKeys(userName);
+            txt_userpassword.SendKeys(passWord);
+            btn_Login.Click();
+        }
+    }
+}
