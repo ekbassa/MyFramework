c095a9cbb273ac897c87cd3a80f0c264f09bd785
Bassam Eketeh <bassameketeh@hotmail.com>
2019-10-07 13:10:24 +0300

    Bassam Config Changes

---
+++ TcoTestingFrameWork/BassamScipts/HelloClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcoTestingFrameWork.BassamScipts
+{
+    class HelloClass
+    {
+        public void SentMes()
+        {
+            Console.WriteLine("hello World");
+
+        }
+    }
+}
